8780618ce12c2c0720ec6d89c7ad7f2776c87b75
Roxanne Landry <roxanne.landry.2@ulaval.ca>
2015-09-15 20:35:16 -0400

    J'ai paufiné login.

--- js/mainPage.js
+++ js/mainPage.js
@@ -1,33 +1,6 @@
 /**
  * Created by RoxanneLandry on 15-09-13.
  */
-function activeDisplay(){
-    var affichage = document.getElementById('navbarSmall');
-    var button = document.getElementById('buttonBurger');
-    alert(button === document.activeElement);
-   if (affichage.style.display == "none" && button === document.activeElement){
-       affichage.style.display = "block";
-       alert(button === document.activeElement);
-   }
-   else{
-       affichage.style.display = "none";
-       alert(button === document.activeElement);
-       button.blur();
-       alert(button === document.activeElement);
-   }
-}
-
-function afficherSearch(id)
-{
-    var searchBar = document.getElementById(id);
-    if (searchBar.style.display == 'none'){
-        searchBar.style.display = 'inline';
-        searchBar.focus();
-    }
-    else{
-        searchBar.style.display = 'none';
-    }
-}
 $(document).ready(function(){
     $('.carouselMultiple').slick({
         infinite: true,
