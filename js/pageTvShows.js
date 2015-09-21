05901aca8f933a9359dd5aaace6fd14144ce62bc
Eliane Trudel <eliane.trudel.1@ulaval.ca>
2015-09-21 11:21:25 -0400

    Size images

--- js/pageTvShows.js
+++ js/pageTvShows.js
@@ -2,54 +2,63 @@
  * Created by Eliane on 2015-09-16.
  */
 
-/*function player() {
+function player() {
     window.open("pageVideo.html", 'Trailer', 'menubar=no, scrollbars=no, top=100, left=100, width=600, height=400');
-}*/
+}
 
 
 $(document).ready(function () {
     $('.carouselMultiple').slick({
         infinite: true,
-        focusOnSelect: false,
-        slidesToShow: 9,
+        adaptiveWidth: true,
+        slidesToShow: 7,
         slidesToScroll: 1,
-        speed: 300,
         responsive: [
             {
                 breakpoint: 1194,
                 settings: {
-                    slidesToShow: 8,
+                    slidesToShow: 6,
                     slidesToScroll: 1
+
                 }
             },
             {
                 breakpoint: 1029,
-                settings: {
-                    slidesToShow: 7,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 999,
-                settings: {
-                    slidesToShow: 6,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 969,
                 settings: {
                     slidesToShow: 5,
                     slidesToScroll: 1
                 }
             },
             {
-                breakpoint: 768,
+                breakpoint: 999,
                 settings: {
                     slidesToShow: 4,
                     slidesToScroll: 1
                 }
+            },
+            {
+                breakpoint: 969,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
+
+
         ]
     });
 });
