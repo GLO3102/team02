95073adefa4d2fdddfe5a8a5a966c817730984d7
Eliane Trudel <eliane.trudel.1@ulaval.ca>
2015-09-22 14:53:08 -0400

    mise à jour page tvShows

--- js/pageTvShows.js
+++ js/pageTvShows.js
@@ -6,3 +6,65 @@ function player() {
     window.open("pageVideo.html", 'Trailer', 'menubar=no, scrollbars=no, top=100, left=100, width=600, height=400');
 }
 
+$(document).ready(function () {
+    $('.carouselMultiple').slick({
+        infinite: true,
+        adaptiveWidth: true,
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1194,
+                settings: {
+                    slidesToShow: 7,
+                    slidesToScroll: 1
+
+                }
+            },
+            {
+                breakpoint: 1029,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 999,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 969,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+
+        ]
+    });
+});
