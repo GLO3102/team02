101eba8a2410f43883a35c02a3b0bb869e81cf6d
Roxanne Landry <roxanne.landry.2@ulaval.ca>
2015-09-25 01:32:24 -0400

    Mis vrai movie pour actor

--- js/pageActor.js
+++ js/pageActor.js
@@ -3,14 +3,14 @@ function AddMovieRow() {
         $("#listMovie")
             .append(
             "<tr>"
-            + "<td><img class='imageList' src='../images/Lorax.jpg'><a class='linkUnderline' href='pageVideo.html'>The lorax</a></td>"
-            + "<td class='pullright'>2014-01-01</td>"
+            + "<td><img class='imageList' src='../images/Movies/Blended.png'><a class='linkUnderline' href='pageVideo.html'>Blended</a></td>"
+            + "<td class='pullright'>2014-05-21</td>"
             + "</tr>");
         $("#listMovie")
             .append(
             "<tr class='bigestOpacity'>"
-            + "<td><img class='imageList' src='../images/Lorax.jpg'><a class='linkUnderline' href='pageVideo.html'>The lorax</a></td>"
-            + "<td class='pullright'>2014-01-01</td>"
+            + "<td><img class='imageList' src='../images/Movies/GrownUps.png'><a class='linkUnderline' href='pageVideo.html'>Grown Up</a></td>"
+            + "<td class='pullright'>2010-11-05</td>"
             + "</tr>");
     }
 }
@@ -19,14 +19,14 @@ function AddTvShowRow() {
         $("#listTvShow")
             .append(
                 "<tr>"
-                + "<td><img class='imageList' src='../images/Lorax.jpg'><a class='linkUnderline' href='pageVideo.html'>The lorax</a></td>"
-                + "<td class='pullright'>2014-01-01</td>"
+                + "<td><img class='imageList' src='../images/BlankImg.png'><a class='linkUnderline' href='pageVideo.html'>Tv show name</a></td>"
+                + "<td class='pullright'>Date</td>"
                 + "</tr>");
         $("#listTvShow")
             .append(
-                "<tr class='bigestOpacity'>"
-                + "<td><img class='imageList' src='../images/Lorax.jpg'><a class='linkUnderline' href='pageVideo.html'>The lorax</a></td>"
-                + "<td class='pullright'>2014-01-01</td>"
+                "<tr>"
+                + "<td><img class='imageList' src='../images/BlankImg.png'><a class='linkUnderline' href='pageVideo.html'>Tv show name</a></td>"
+                + "<td class='pullright'>Date</td>"
                 + "</tr>");
 
     }
