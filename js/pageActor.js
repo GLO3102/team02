6898edeb1c182e4a8991082189596a28848284c6
Roxanne Landry <roxanne.landry.2@ulaval.ca>
2015-09-25 00:55:56 -0400

    Link movie/tv list in Actors

--- js/pageActor.js
+++ js/pageActor.js
@@ -3,13 +3,13 @@ function AddMovieRow() {
         $("#listMovie")
             .append(
             "<tr>"
-            + "<td><img class='imageList' src='../images/Lorax.jpg'>The lorax</td>"
+            + "<td><img class='imageList' src='../images/Lorax.jpg'><a class='linkUnderline' href='pageVideo.html'>The lorax</a></td>"
             + "<td class='pullright'>2014-01-01</td>"
             + "</tr>");
         $("#listMovie")
             .append(
             "<tr class='bigestOpacity'>"
-            + "<td><img class='imageList' src='../images/Lorax.jpg'>The lorax</td>"
+            + "<td><img class='imageList' src='../images/Lorax.jpg'><a class='linkUnderline' href='pageVideo.html'>The lorax</a></td>"
             + "<td class='pullright'>2014-01-01</td>"
             + "</tr>");
     }
@@ -19,13 +19,13 @@ function AddTvShowRow() {
         $("#listTvShow")
             .append(
                 "<tr>"
-                + "<td><img class='imageList' src='../images/Lorax.jpg'>The lorax</td>"
+                + "<td><img class='imageList' src='../images/Lorax.jpg'><a class='linkUnderline' href='pageVideo.html'>The lorax</a></td>"
                 + "<td class='pullright'>2014-01-01</td>"
                 + "</tr>");
         $("#listTvShow")
             .append(
                 "<tr class='bigestOpacity'>"
-                + "<td><img class='imageList' src='../images/Lorax.jpg'>The lorax</td>"
+                + "<td><img class='imageList' src='../images/Lorax.jpg'><a class='linkUnderline' href='pageVideo.html'>The lorax</a></td>"
                 + "<td class='pullright'>2014-01-01</td>"
                 + "</tr>");
 
