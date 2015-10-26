61551158e2141f0a5d3899da8070e2de02381d69
Roxanne Landry <roxanne.landry.2@ulaval.ca>
2015-10-26 19:14:01 -0400

    Template

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-/**
- * Created by RoxanneLandry on 15-10-26.
- */
+require.config({
+    paths: {
+        jquery: 'libs/jquery/jquery-2.1.4',
+        underscore: 'libs/underscore/underscore',
+        backbone: 'libs/backbone/backbone',
+        slick: 'libs/slick-1.5.7/slick/slick.js',
+        MainPageView :'views/MainPageView',
+        text: 'libs/require/text'
+    }
+
+});
+
+require([
+    'app'
+], function(App){
+    App.initialize();
+});
--- js/router.js
+++ js/router.js
@@ -1,3 +1,29 @@
 /**
  * Created by RoxanneLandry on 15-10-26.
  */
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'MainPageView'
+], function($, _, Backbone, MainPageView){
+    var AppRouter = Backbone.Router.extend({
+        routes: {
+            '':'home'
+        }
+    });
+
+    var initialize = function(){
+        var app_router = new AppRouter;
+
+        app_router.on('route:home', function(){
+            var mainPageView  = new MainPageView();
+            mainPageView.render();
+        });
+
+        Backbone.history.start();
+    };
+    return {
+        initialize: initialize
+    };
+});
