b8b340305e1878bdd08416445acb704195cd99b6
Maxime Pare <maxime.pare.4@ulaval.ca>
2015-11-01 23:05:30 -0500

    actor template

--- js/views/ActorView.js
+++ js/views/ActorView.js
@@ -1,4 +1,43 @@
 /**
  * Created by RoxanneLandry on 15-11-01.
  */
-//Je suis un test
+/**
+ * Created by Maxime on 2015-10-30.
+ */
+/**
+ * Created by Maxime on 2015-10-30.
+ */
+define([
+    'jquery',
+    'bootstrap',
+    'presentation',
+    'underscore',
+    'backbone',
+    'models/Actor',
+    'text!templates/actor.html'
+
+], function($, _bootstrap, presentation,  _, Backbone, Actor, actorTemplate){
+    var ActorView = Backbone.View.extend({
+        template: _.template(actorTemplate),
+
+        el: $(".content"),
+        initialize: function(){
+            this.acteurInfo = new Actor();
+
+            this.acteurInfo.fetch({
+                success: function(acteurInfo){
+                },
+                error: function(){
+                }
+
+            });
+
+        },
+        render: function() {
+            this.$el.html(this.template({actorInfo: this.acteurInfo.toJSON()}));
+        }
+    });
+
+
+    return ActorView;
+});
